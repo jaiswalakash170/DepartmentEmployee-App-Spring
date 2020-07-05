18450db99e6c7f403d1758be37b31a9dc02ed828
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2020-07-05 22:10:56 +0530

    Department Employee Spring Boot App initial commit with Model classes Employee & Department

---
+++ src/main/java/com/akash/depemp/DepartmentEmployeeAppApplication.java
@@ -0,0 +1,13 @@
+package com.akash.depemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentEmployeeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentEmployeeAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/akash/depemp/models/Employee.java
@@ -0,0 +1,45 @@
+package com.akash.depemp.models;
+
+public class Employee {
+	private int EmployeeId;
+	private String EmployeeName;
+	private String Department;
+	private String MailID;
+	private String DOJ;
+	public int getEmployeeId() {
+		return EmployeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		EmployeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return EmployeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		EmployeeName = employeeName;
+	}
+	public String getDepartment() {
+		return Department;
+	}
+	public void setDepartment(String department) {
+		Department = department;
+	}
+	public String getMailID() {
+		return MailID;
+	}
+	public void setMailID(String mailID) {
+		MailID = mailID;
+	}
+	public String getDOJ() {
+		return DOJ;
+	}
+	public void setDOJ(String dOJ) {
+		DOJ = dOJ;
+	}
+	@Override
+	public String toString() {
+		return "Employee [EmployeeId=" + EmployeeId + ", EmployeeName=" + EmployeeName + ", Department=" + Department
+				+ ", MailID=" + MailID + ", DOJ=" + DOJ + "]";
+	}
+	
+}
